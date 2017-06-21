d4f2554dd5b19d4e1d23e1346b52b8d936e0075e
Marks Polakovs <marks@markspolakovs.me>
2017-06-21 16:35:08 +0200

    Make less shit

--- parallax.js
+++ parallax.js
@@ -1,45 +1,9 @@
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    document.getElementById("mobile").style.display = "block";
-	document.getElementById("desktop").style.display = "none";
-	
-	
-	function mtcotc() {
-		document.getElementById("mtcotc").style.display = "block";
-		document.getElementById("medite").style.display = "none";
-	}
-
-	function medite() {
-		document.getElementById("mtcotc").style.display = "none";
-		document.getElementById("medite").style.display = "block";
-	}
-
-	
+function dtcotc() {
+	document.getElementById("dtcotc").style.display = "block";
+	document.getElementById("dedite").style.display = "none";
 }
 
-else {
-	document.getElementById("desktop").style.display = "block";
-	document.getElementById("mobile").style.display = "none";
-	
-	
-	
-	function dtcotc() {
-		document.getElementById("dtcotc").style.display = "block";
-		document.getElementById("dedite").style.display = "none";
-	}
-
-	function dedite() {
-		document.getElementById("dtcotc").style.display = "none";
-		document.getElementById("dedite").style.display = "block";
-	}
+function dedite() {
+	document.getElementById("dtcotc").style.display = "none";
+	document.getElementById("dedite").style.display = "block";
 }
-
-
-
-
-
-
-
-
-
-
